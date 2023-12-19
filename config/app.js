fecec7f36792c592393b6fff04b2301c3ceea768
marielaure1 <edjour.marielaure@gmail.com>
2023-12-20 00:52:02 +0100

    add config

---
+++ config/app.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+    database: {
+      user: process.env.DB_USER || 'votre_utilisateur',
+      host: process.env.DB_HOST || 'localhost',
+      database: process.env.DB_NAME || 'votre_base_de_donnees',
+      password: process.env.DB_PASSWORD || 'votre_mot_de_passe',
+      port: process.env.DB_PORT || 5432,
+    },
+};
+  
